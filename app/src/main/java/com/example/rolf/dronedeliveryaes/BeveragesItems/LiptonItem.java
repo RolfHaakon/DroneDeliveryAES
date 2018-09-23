17f537b45d6b5e395137ebd5fe7c90629648f0a3
robert daniel cook <robertdaniel.cook@my.jcu.edu.com>
2018-09-23 19:07:06 +0800

    Created beverages category and items shells

---
+++ app/src/main/java/com/example/rolf/dronedeliveryaes/BeveragesItems/LiptonItem.java
@@ -0,0 +1,15 @@
+package com.example.rolf.dronedeliveryaes.BeveragesItems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rolf.dronedeliveryaes.R;
+
+public class LiptonItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lipton_item);
+    }
+}
---
+++ app/src/main/java/com/example/rolf/dronedeliveryaes/BeveragesItems/WaterItem.java
@@ -0,0 +1,15 @@
+package com.example.rolf.dronedeliveryaes.BeveragesItems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rolf.dronedeliveryaes.R;
+
+public class WaterItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_item);
+    }
+}
