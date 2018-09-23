5fbd742c6b5a4cad49afc3c9c3973e9404c9b0d1
Rolf <30429371+RolfHaakon@users.noreply.github.com>
2018-09-24 01:53:50 +0800

    added order history

--- app/src/main/java/com/example/rolf/dronedeliveryaes/activities/OrderHistory.java
+++ app/src/main/java/com/example/rolf/dronedeliveryaes/activities/OrderHistory.java
@@ -1,72 +1,54 @@
 package com.example.rolf.dronedeliveryaes.activities;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.UserManager;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 import android.widget.Button;
+
 import com.example.rolf.dronedeliveryaes.R;
 import com.example.rolf.dronedeliveryaes.model.User;
-import com.example.rolf.dronedeliveryaes.sql.DatabaseHelper;
 
 import java.util.List;
 
-
-public class Account extends AppCompatActivity {
+public class OrderHistory extends AppCompatActivity{
 
     private Button Category;
     private Button Track;
     private Button Home;
     private Button Cart;
-    private Button OrderHistory;
-    private AppCompatTextView textViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_order_history);
 
         Category = (Button) findViewById(R.id.category);
         Track = (Button) findViewById(R.id.track);
         Home = (Button)  findViewById(R.id.home);
         Cart = (Button) findViewById(R.id.cart);
-        OrderHistory = (Button) findViewById(R.id.orderHistory);
-        initViews();
-        initObjects();
 
 
-
-        OrderHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToOrderHistory = new Intent(Account.this, OrderHistory.class);
-                startActivity(goToOrderHistory);
-            }
-        });
 
         Category.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToCategory = new Intent(Account.this, Category.class);
+                Intent goToCategory = new Intent(OrderHistory.this, Category.class);
                 startActivity(goToCategory);
             }
         });
         Track.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHomePage = new Intent(Account.this, Track.class);
+                Intent goToHomePage = new Intent(OrderHistory.this, Track.class);
                 startActivity(goToHomePage);
             }
         });
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHomePage = new Intent(Account.this, HomePage.class);
+                Intent goToHomePage = new Intent(OrderHistory.this, HomePage.class);
                 startActivity(goToHomePage);
             }
         });
@@ -74,22 +56,13 @@ public class Account extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent goToShoppingCart = new Intent(Account.this, ShoppingCart.class);
+                Intent goToShoppingCart = new Intent(OrderHistory.this, ShoppingCart.class);
                 startActivity(goToShoppingCart);
 
             }
         });
 
 
-    }
-
-
-    private void initObjects() {
-        textViewName.setText(HomePage.Current_User);
-    }
-
-    private void initViews() {
-        textViewName = (AppCompatTextView) findViewById(R.id.textViewName);
     }
 
 }
