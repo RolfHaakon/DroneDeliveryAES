04eb10310a56223fd02519e371955b71ef172b0b
robert daniel cook <robertdaniel.cook@my.jcu.edu.com>
2018-09-23 21:41:48 +0800

    Fixed shopping cart navigation and added DeliveryTime class

---
+++ app/src/main/java/com/example/rolf/dronedeliveryaes/activities/DeliveryTime.java
@@ -0,0 +1,15 @@
+package com.example.rolf.dronedeliveryaes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rolf.dronedeliveryaes.R;
+
+public class DeliveryTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_time);
+    }
+}
